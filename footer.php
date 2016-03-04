44cdd20d501350daaa42ff6a3788903f6d3bd264
Alexander <somin.oleksandr@gmail.com>
2016-03-04 03:17:35 +0200

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div><!-- / end page container, begun in the header -->
+
+
+<footer>
+	
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,7 @@
+<?php get_header(); // This fxn gets the header.php file and renders it ?>
+
+
+
+	
+	
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
